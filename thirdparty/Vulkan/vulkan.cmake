387e922c7494dba1f08c3d7a986f287c44d84aa3
HrOokami <faserg11@gmail.com>
2017-10-26 22:31:18 +0300

    Добавляю Vulkan #2

--- thirdparty/Vulkan/vulkan.cmake
+++ thirdparty/Vulkan/vulkan.cmake
@@ -1,1 +1,10 @@
 cmake_minimum_required (VERSION 2.8)
+
+set (VULKAN_SDK_INCLUDE $ENV{VULKAN_SDK}/Include)
+
+if (TARGET_ARCH STREQUAL x86)
+	set (VULKAN_SDK_LIB $ENV{VULKAN_SDK}/Lib32 )
+else ()
+	set (VULKAN_SDK_LIB $ENV{VULKAN_SDK}/Lib )
+endif ()
+
