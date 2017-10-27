941fcb17127ecd1ec19b458b4f0cf839df1936da
HrOokami <faserg11@gmail.com>
2017-10-27 21:47:45 +0300

    Все библиотеки подключены. Close #2.

--- CubA4Loader/src/main.cpp
+++ CubA4Loader/src/main.cpp
@@ -1,6 +1,9 @@
 #include <SDL.h>
+#include <phonon.h>
 
 int main(int argc, char *argv[])
 {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Quit();
 	return 0;
 }
--- thirdparty/SDL/sdl.cmake
+++ thirdparty/SDL/sdl.cmake
@@ -5,10 +5,3 @@ endif (MSVC)
 
 set (SDL_INCLUDE_DIR ${SDL2_PATH}/include  )
 set (SDL_LIBRARY_DIR ${SDL2_PATH}/lib/${TARGET_ARCH})
-
-#add_library( SDL2 SHARED IMPORTED )
-#add_library( SDL2main SHARED IMPORTED )
-#add_library( SDL2test SHARED IMPORTED )
-#set_target_properties( SDL2 PROPERTIES IMPORTED_LOCATION ${SDL_LIBRARY_DIR} )
-#set_target_properties( SDL2main PROPERTIES IMPORTED_LOCATION ${SDL_LIBRARY_DIR} )
-#set_target_properties( SDL2test PROPERTIES IMPORTED_LOCATION ${SDL_LIBRARY_DIR} )
