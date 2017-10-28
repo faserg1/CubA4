66474fe5c8d569f6218a27be5dac8ef766e6a781
HrOokami <faserg11@gmail.com>
2017-10-28 17:33:16 +0300

    Работаем над архитекторой. Тазик #1.

---
+++ ModLoader/include/loader/ModLoader.hpp
@@ -0,0 +1,19 @@
+#ifndef LOADER_MOD_LOADER
+#define LOADER_MOD_LOADER
+
+#include <ModLoaderLibrary.hpp>
+
+namespace CubA4
+{
+	namespace mod_loader
+	{
+		class MOD_LOADER_SHARED ModLoader
+		{
+		public:
+			ModLoader();
+			virtual ~ModLoader();
+		};
+	}
+}
+
+#endif
---
+++ ModLoader/src/loader/ModLoader.cpp
@@ -0,0 +1,11 @@
+#include <loader/ModLoader.hpp>
+
+using namespace CubA4::mod_loader;
+
+ModLoader::ModLoader()
+{
+}
+
+ModLoader::~ModLoader()
+{
+}
