9b0d205b4f86c430d0ed04ff7dab8d7245e54978
HrOokami <faserg11@gmail.com>
2018-05-11 22:39:44 +0300

    Теперь можно начать рекурсивный поиск по папкам.
    Issue #17.

--- util/cmake_patcher.py
+++ util/cmake_patcher.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import os
+import re
+from .lineutil import *
+
+RE_NC_P = "(?!^#)"
+RE_BEGIN = re.compile(RE_NC_P + "cmake_minimum_required\\s*\\(VERSION.*\\)")
 
 class CMakePatcher:
 	def __init__(self, module_name, header_path, source_path):
@@ -12,15 +17,56 @@ class CMakePatcher:
 		self._read()
 	
 	def patch(self):
-		pass
+		self._cmake_starts()
+		#self._root_folders()
+	
+	def _cmake_starts(self):
+		self._cmake_begin_index = self._find(RE_BEGIN)
+		if self._cmake_begin_index < 0:
+			self._insert(0, "cmake_minimum_required (VERSION 2.8)\n")
+			self._cmake_begin_index = self._find(RE_BEGIN)
+	
+	def _convert_folders(self, folders):
+		full_folders = ""
+		for folder in folders:
+			full_folders += "Source" if folder == "src" else camel_case(folder)
+		return full_folders
+		
+	def _full_folders_name(self, folders):
+		return self._module_name + self._convert_folders(folders) + "Dir"
+	
+	def _prev_folder_name(self, folders):
+		if len(folders) > 1:
+			return self._full_folders_name(folders[:-1])
+		return "CMAKE_CURRENT_LIST_DIR"
+	
+	def _folder(self, folders, index_after):
+		full_name = self._full_folders_name(folders)
+		re_folder = re.compile(RE_NC_P + "set\\s*\\(\\s*" + full_name + "\\s+.*/\\)")
+		index = self._find(re_folder)
+		if index < 0:
+			last_folder = folders[-1]
+			last_folder_path = "${" + self._prev_folder_name(folders) + "}/" + last_folder
+			self._insert(index_after, "set(" + full_name + " " + last_folder_path + ")")
+			return self._find(re_folder)
+		return index
 	
 	def _read(self):
 		cmake_path = os.path.join(self._cwd, self._module_name, "CMakeLists.txt")
 		with open(cmake_path, "r") as cmake_file:
 			self._readed_cmake = cmake_file.read()
+			
+	def _find(self, pattern):
+		return find_line_by_pattern(self._readed_cmake, pattern)
+		
+	def _insert(self, index, text):
+		self._readed_cmake = insert_text(self._readed_cmake, text, index)
+		return self._readed_cmake
 	
 	_readed_cmake = ""
 	_module_name = ""
 	_header_path = ""
 	_source_path = ""
 	_cwd = ""
+	
+	_cmake_begin_index = 0
--- util/lineutil.py
+++ util/lineutil.py
@@ -3,18 +3,21 @@
 def insert_text(source, text, index):
 	return source[:index] + text + source[index:]
 
-def insert_line(file_path, str, pos):
-	full_file = ""
-	with open(file_path, "r") as file:
-		full_file = file.read()
-	full_file = insert_text(full_file, str, pos)
-	
-	with open(file_path, "w") as file:
-		file.write(full_file)
-
-def find_line_by_pattern(file_path, compiled_re):
-	full_file = ""
-	with open(file_path, "r") as file:
-		full_file = file.read()
+def find_line_by_pattern(full_file, compiled_re):
 	match = compiled_re.search(full_file)
+	if not match:
+		return -1
 	return match.end(0)
+	
+def camel_case(input_str, first_drop = False):
+	output_str = ""
+	need_upper = not first_drop
+	for symbol in input_str:
+		if symbol == "_":
+			need_upper = True
+			continue
+		if need_upper:
+			symbol = symbol.upper()
+			need_upper = False
+		output_str += symbol
+	return output_str
