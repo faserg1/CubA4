6fbf9ce93f19e13cd89cea3473821148935538db
HrOokami <faserg11@gmail.com>
2018-05-18 01:10:44 +0300

    Продвигаем логгирование.
    Issue #16.

--- Core/src/logging/Logger.cpp
+++ Core/src/logging/Logger.cpp
@@ -1,5 +1,30 @@
 #include "../../include/logging/Logger.hpp"
+#include <boost/filesystem.hpp>
+#include <ostream>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/device/file.hpp>
 using namespace CubA4::core::logging;
+
+namespace CubA4
+{
+	namespace core
+	{
+		namespace logging
+		{
+			class LoggerStreams
+			{
+				boost::iostreams::stream_buffer<boost::iostreams::file_sink> buf_;
+				std::ostream out_;
+
+				friend class Logger;
+				LoggerStreams(std::string filePath) : buf_(filePath), out_(&buf_)
+				{
+
+				}
+			};
+		}
+	}
+}
 
 Logger::Logger()
 {
@@ -9,5 +34,15 @@ Logger::Logger()
 Logger::~Logger()
 {
 	
+}
+
+std::string CubA4::core::logging::Logger::getNextLogName()
+{
+	return std::string();
+}
+
+void CubA4::core::logging::Logger::openLogStream()
+{
+	stream_ = std::make_unique<LoggerStreams>(getNextLogName());
 }
 
