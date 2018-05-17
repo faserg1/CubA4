3fd81fd2b7d74af8ee9ba635ca55c94d50eb5d64
HrOokami <faserg11@gmail.com>
2018-05-18 01:10:19 +0300

    Пишем интерфейс для поиска папок с нужными файлами.
    Issue #13.

---
+++ CoreBase/include/config/IFilePaths.hpp
@@ -0,0 +1,28 @@
+#ifndef IFILEPATHS_HPP
+#define IFILEPATHS_HPP
+
+#include <string>
+
+namespace CubA4
+{
+	namespace core
+	{
+		namespace config
+		{
+			class IFilePaths
+			{
+			public:
+				virtual std::string configPath() const = 0;
+				virtual std::string modsPath() const = 0;
+				virtual std::string renderPath() const = 0;
+				virtual std::string logsPath() const = 0;
+			protected:
+				explicit IFilePaths() {}
+				virtual ~IFilePaths() {}
+			private:
+			};
+		}
+	}
+}
+
+#endif // IFILEPATHS_HPP
