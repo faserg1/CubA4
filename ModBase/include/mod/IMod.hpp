d9221290d2dad8cc6595f63e9c2a0658096b5f3a
faserg1 <faserg11@gmail.com>
2018-05-25 16:07:49 +0300

    Модификации.
    Issue #9.

--- ModBase/include/mod/IMod.hpp
+++ ModBase/include/mod/IMod.hpp
@@ -1,8 +1,18 @@
 #ifndef IMOD_HPP
 #define IMOD_HPP
 
+#include <memory>
+
 namespace CubA4
 {
+	namespace core
+	{
+		namespace logging
+		{
+			class ILogger;
+		}
+	}
+	
 	namespace mod
 	{
 		enum class ModState : char
@@ -24,9 +34,9 @@ namespace CubA4
 		public:
 			virtual ~IMod() {}
 
-			virtual void load() = 0;
+			virtual void load(std::shared_ptr<core::logging::ILogger> logger) = 0;
 			virtual void preinit() = 0;
-			virtual void link(const IModLinker *linker) = 0;
+			virtual void link(std::shared_ptr<const IModLinker> linker) = 0;
 			virtual void init() = 0;
 			virtual void configure() = 0;
 			virtual void done() = 0;
---
+++ ModBase/include/mod/IModLinker.hpp
@@ -0,0 +1,19 @@
+#ifndef IMODLINKER_HPP
+#define IMODLINKER_HPP
+
+namespace CubA4
+{
+	namespace mod
+	{
+		class IModLinker
+		{
+		public:
+			virtual ~IModLinker() {}
+		protected:
+			explicit IModLinker() {}
+		private:
+		};
+	}
+}
+
+#endif // IMODLINKER_HPP
