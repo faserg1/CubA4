db7f5ec79aec26a23acfebd9a51188f5131c5694
HrOokami <faserg11@gmail.com>
2018-06-08 00:31:14 +0300

    Регистрация в основном файле.
    Issue #38.

--- util/compile_internal_resources.py
+++ util/compile_internal_resources.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
+from .resource_generator import ResourceGenerator
+
 projects = ["RenderVulkan", "ModVanilla"]
+
+for project in projects:
+	rg = ResourceGenerator(project)
+	rg.generate()
