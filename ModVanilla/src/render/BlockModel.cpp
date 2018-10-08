3522341bec09bbd8aad136a4bd6cd32a756d1502
HrOokami <faserg11@gmail.com>
2018-10-08 22:14:46 +0300

    Индексы блока: развертка.
    Issue #66.

--- ModVanilla/src/render/BlockModel.cpp
+++ ModVanilla/src/render/BlockModel.cpp
@@ -16,19 +16,26 @@ std::vector<CubA4::core::model::Vertex> BlockModel::getVertices() const
 	return
 	{
 		//up
-		{0.5,0.5,0.5},
-		{0.5,-0.5,0.5},
-		{-0.5,-0.5,0.5},
-		{-0.5,0.5,0.5},
+		{0.5,0.5,0.5},		// front right
+		{0.5,-0.5,0.5},		// back right
+		{-0.5,-0.5,0.5},	// back left
+		{-0.5,0.5,0.5},		// front left
 		//down
-		{0.5,0.5,-0.5},
-		{0.5,-0.5,-0.5},
-		{-0.5,-0.5,-0.5},
-		{-0.5,0.5,-0.5},
+		{0.5,0.5,-0.5},		// front righ
+		{0.5,-0.5,-0.5},	// back right
+		{-0.5,-0.5,-0.5},	// back left
+		{-0.5,0.5,-0.5},	// front left
 	};
 }
 
 std::vector<unsigned short> BlockModel::getFaceIndices() const
 {
-	return {};
+	return {
+		4, 0, 1, 2, 3, //up
+		4, 4, 5, 6, 7, //down
+		4, 0, 3, 4, 7, //front
+		4, 1, 2, 5, 6, //back
+		4, 0, 1, 4, 5, //right
+		4, 2, 3, 6, 7 //left
+	};
 }
--- RenderVulkan/src/engine/model/ModelManager.cpp
+++ RenderVulkan/src/engine/model/ModelManager.cpp
@@ -38,6 +38,8 @@ std::shared_ptr<const IRenderModel> ModelManager::registerModel(const CubA4::cor
 		bufferInfos[bufferIndex].sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 	}
 
+	//-------------- Vertex
+
 	VkBufferCreateInfo &vertexBufferInfo = bufferInfos[0], &vertexTransitBufferInfo = bufferInfos[1];
 	vertexTransitBufferInfo.size = vertexBufferInfo.size = verticesSize;
 	vertexTransitBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
@@ -52,6 +54,9 @@ std::shared_ptr<const IRenderModel> ModelManager::registerModel(const CubA4::cor
 		// TODO: [OOKAMI] Exception, etc
 	}
 
+	device_->getMarker().setName(vertexTransitBuffer, "vertex transit buffer");
+	device_->getMarker().setName(vertexBuffer, "vertex buffer");
+
 	VkBufferMemoryRequirementsInfo2 reqVertexBufferInfo = {}, reqVertexTransitBufferInfo = {};
 	reqVertexBufferInfo.sType = reqVertexTransitBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_REQUIREMENTS_INFO_2;
 	reqVertexTransitBufferInfo.buffer = vertexTransitBuffer;
@@ -59,6 +64,7 @@ std::shared_ptr<const IRenderModel> ModelManager::registerModel(const CubA4::cor
 
 	VkMemoryRequirements2 reqVertexBuffer = {}, reqVertexTransitBuffer = {};
 	reqVertexBuffer.sType = reqVertexTransitBuffer.sType = VK_STRUCTURE_TYPE_MEMORY_REQUIREMENTS_2;
+
 	vkGetBufferMemoryRequirements2(device_->getDevice(), &reqVertexTransitBufferInfo, &reqVertexTransitBuffer);
 	vkGetBufferMemoryRequirements2(device_->getDevice(), &reqVertexBufferInfo, &reqVertexBuffer);
 
@@ -67,14 +73,106 @@ std::shared_ptr<const IRenderModel> ModelManager::registerModel(const CubA4::cor
 	auto memoryVertexBuffer = allocator_->allocate(reqVertexBuffer.memoryRequirements.size,
 		MemoryAllocationPrefered::Device, reqVertexBuffer.memoryRequirements.memoryTypeBits);
 
-	auto mappedMemory = memoryVertexTransitBuffer->map(0, VK_WHOLE_SIZE);
-	memcpy(mappedMemory.get(), vertices.data(), verticesSize);
-	mappedMemory.reset();
+	auto mappedVertexMemory = memoryVertexTransitBuffer->map(0, VK_WHOLE_SIZE);
+	memcpy(mappedVertexMemory.get(), vertices.data(), verticesSize);
+	mappedVertexMemory.reset();
 
 	vkBindBufferMemory(device_->getDevice(), vertexTransitBuffer, memoryVertexTransitBuffer->getMemory(), 0);
 	vkBindBufferMemory(device_->getDevice(), vertexBuffer, memoryVertexBuffer->getMemory(), 0);
 
 	memoryManager_->copyBufferToBuffer(vertexTransitBuffer, vertexBuffer, verticesSize);
+
+	vkDestroyBuffer(device_->getDevice(), vertexTransitBuffer, nullptr);
+
+	//-------------- Index
+
+	for (size_t faceIndex = 0; faceIndex < faces.size(); faceIndex++)
+	{
+		auto verticesCount = faces[faceIndex];
+		faceIndex += verticesCount;
+		indicesSize += (verticesCount - 2) * 3; // triangle fan
+	}
+
+	VkBufferCreateInfo &indexBufferInfo = bufferInfos[2], &indexTransitBufferInfo = bufferInfos[3];
+	indexTransitBufferInfo.size = indexBufferInfo.size = indicesSize;
+	indexTransitBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+	indexBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+
+	if (vkCreateBuffer(device_->getDevice(), &indexTransitBufferInfo, nullptr, &indexTransitBuffer) != VK_SUCCESS)
+	{
+		// TODO: [OOKAMI] Exception, etc
+	}
+	if (vkCreateBuffer(device_->getDevice(), &indexBufferInfo, nullptr, &indexBuffer) != VK_SUCCESS)
+	{
+		// TODO: [OOKAMI] Exception, etc
+	}
+
+	device_->getMarker().setName(indexTransitBuffer, "index transit buffer");
+	device_->getMarker().setName(indexBuffer, "index buffer");
+	
+	VkBufferMemoryRequirementsInfo2 reqIndexBufferInfo = {}, reqIndexTransitBufferInfo = {};
+	reqIndexBufferInfo.sType = reqIndexTransitBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_REQUIREMENTS_INFO_2;
+	reqIndexTransitBufferInfo.buffer = indexTransitBuffer;
+	reqIndexBufferInfo.buffer = indexBuffer;
+
+	VkMemoryRequirements2 reqIndexBuffer = {}, reqIndexTransitBuffer = {};
+	reqIndexBuffer.sType = reqIndexTransitBuffer.sType = VK_STRUCTURE_TYPE_MEMORY_REQUIREMENTS_2;
+
+	vkGetBufferMemoryRequirements2(device_->getDevice(), &reqIndexTransitBufferInfo, &reqIndexTransitBuffer);
+	vkGetBufferMemoryRequirements2(device_->getDevice(), &reqIndexBufferInfo, &reqIndexBuffer);
+
+	
+	auto memoryIndexTransitBuffer = allocator_->allocate(reqIndexTransitBuffer.memoryRequirements.size,
+		MemoryAllocationPrefered::Host, reqIndexTransitBuffer.memoryRequirements.memoryTypeBits);
+	auto memoryIndexBuffer = allocator_->allocate(reqIndexBuffer.memoryRequirements.size,
+		MemoryAllocationPrefered::Device, reqIndexBuffer.memoryRequirements.memoryTypeBits);
+
+	
+	auto mappedIndexMemory = memoryIndexTransitBuffer->map(0, VK_WHOLE_SIZE);
+	auto *idxPtr = static_cast<unsigned short*>(mappedIndexMemory.get());
+	unsigned short triangleIndices[3] = {};
+	auto saveTriangle = [&idxPtr, &triangleIndices]()
+	{
+		memcpy(idxPtr, triangleIndices, sizeof(triangleIndices));
+		idxPtr += 3;
+	};
+	for (size_t faceIndex = 0, verticesCount = 0, vertexIndex = 0;
+		faceIndex < faces.size();
+		faceIndex++, verticesCount--, vertexIndex++)
+	{
+		if (verticesCount == 0)
+		{
+			verticesCount = faces[faceIndex] + 1; //add + 1 for next iteration
+			vertexIndex = -1; //for next iteration
+		}
+		else
+		{
+			//save first triangle
+			if (vertexIndex == 3)
+			{
+				saveTriangle();
+			}
+			//record first triangle
+			if (vertexIndex < 3)
+			{
+				triangleIndices[vertexIndex] = static_cast<unsigned short>(faces[faceIndex]);
+			}
+			else //record next triangles
+			{
+				triangleIndices[1] = triangleIndices[2];
+				triangleIndices[2] = static_cast<unsigned short>(faces[faceIndex]);
+				saveTriangle();
+			}
+		}
+	}
+	mappedIndexMemory.reset();
+
+	vkBindBufferMemory(device_->getDevice(), indexTransitBuffer, memoryIndexTransitBuffer->getMemory(), 0);
+	vkBindBufferMemory(device_->getDevice(), indexBuffer, memoryIndexBuffer->getMemory(), 0);
+
+	memoryManager_->copyBufferToBuffer(indexTransitBuffer, indexBuffer, indicesSize);
+
+	vkDestroyBuffer(device_->getDevice(), indexTransitBuffer, nullptr);
 
 	return {};
 }
