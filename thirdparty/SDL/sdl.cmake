47918406f101510f307aa54ae9d36e13b6e21922
HrOokami <faserg11@gmail.com>
2018-11-21 13:37:38 +0300

    Попытка №3.
    Issue #70.

--- thirdparty/SDL/sdl.cmake
+++ thirdparty/SDL/sdl.cmake
@@ -1,7 +1,9 @@
 cmake_minimum_required (VERSION 2.8)
 if (MSVC)
 	set(SDL2_PATH ${CMAKE_CURRENT_LIST_DIR}/msvc)
-endif (MSVC)
-
-set (SDL_INCLUDE_DIR ${SDL2_PATH}/include  )
-set (SDL_LIBRARY_DIR ${SDL2_PATH}/lib/${TARGET_ARCH})
+	set (SDL_INCLUDE_DIR ${SDL2_PATH}/include)
+	set (SDL_LIBRARY_DIR ${SDL2_PATH}/lib/${TARGET_ARCH})
+else (UNIX)
+	set (SDL_INCLUDE_DIR /usr/include)
+	set (SDL_LIBRARY_DIR /usr/lib/)
+endif ()
