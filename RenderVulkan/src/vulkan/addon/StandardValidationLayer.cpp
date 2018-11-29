bbc2905db68c5aeef79b6dfe200899ca9f90c0db
HrOokami <faserg11@gmail.com>
2018-11-29 20:41:38 +0300

    Implicitly add layers
    Issue #71.

--- RenderVulkan/src/vulkan/addon/StandardValidationLayer.cpp
+++ RenderVulkan/src/vulkan/addon/StandardValidationLayer.cpp
@@ -15,7 +15,18 @@ StandardValidationLayer::~StandardValidationLayer()
 
 std::vector<std::string> StandardValidationLayer::names() const
 {
-	return {"VK_LAYER_LUNARG_standard_validation"};
+	return
+	{
+		//"VK_LAYER_LUNARG_standard_validation"
+		"VK_LAYER_GOOGLE_threading",
+		"VK_LAYER_LUNARG_parameter_validation",
+		"VK_LAYER_LUNARG_device_limits",
+		"VK_LAYER_LUNARG_object_tracker",
+		"VK_LAYER_LUNARG_image",
+		"VK_LAYER_LUNARG_core_validation",
+		"VK_LAYER_LUNARG_swapchain",
+		"VK_LAYER_GOOGLE_unique_objects",
+	};
 }
 
 void StandardValidationLayer::init(std::shared_ptr<const Instance> instance)
