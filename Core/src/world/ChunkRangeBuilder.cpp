c5fbf78ca55d86a5de8cbbb28f3c0da6053505e1
HrOokami <faserg11@gmail.com>
2018-12-23 16:56:44 +0300

    Поиск соседей - сделан для текущей реализации чанка.
    Issue #80.

--- Core/src/world/ChunkRangeBuilder.cpp
+++ Core/src/world/ChunkRangeBuilder.cpp
@@ -16,7 +16,29 @@ std::vector<ChunkRangeBuilder::sChunkRanges> ChunkRangeBuilder::findAdjacent(con
 
 bool ChunkRangeBuilder::isAdjacent(scIChunkRange first, scIChunkRange second)
 {
-	return false;
+	if (first->getBlock() != second->getBlock())
+		return false;
+	auto bounds1 = minMaxBounds(first->getBounds()), bounds2 = minMaxBounds(second->getBounds());
+	uint8_t firstCounter = 0, secondCounter = 0;
+
+	auto test = [](uint8_t &counter, const BlockInChunkPos &min1, const BlockInChunkPos &max2, decltype(BlockInChunkPos::x) BlockInChunkPos::*pos)
+	{
+		// TODO: [OOKAMI] Учитывать, когда чанк может считаться по другому
+		if (max2.*pos <= ChunkSize)
+			counter += (min1.*pos <= max2.*pos + 1 ? 1 : 0);
+		else if (min1.*pos > 0)
+			counter += (min1.*pos - 1 <= max2.*pos ? 1 : 0);
+	};
+
+	test(firstCounter, bounds1[MinIndex], bounds2[MaxIndex], &BlockInChunkPos::x);
+	test(firstCounter, bounds1[MinIndex], bounds2[MaxIndex], &BlockInChunkPos::y);
+	test(firstCounter, bounds1[MinIndex], bounds2[MaxIndex], &BlockInChunkPos::z);
+
+	test(secondCounter, bounds2[MinIndex], bounds1[MaxIndex], &BlockInChunkPos::x);
+	test(secondCounter, bounds2[MinIndex], bounds1[MaxIndex], &BlockInChunkPos::y);
+	test(secondCounter, bounds2[MinIndex], bounds1[MaxIndex], &BlockInChunkPos::z);
+
+	return firstCounter > 1 && secondCounter > 1;
 }
 
 bool ChunkRangeBuilder::isIntersects(scIChunkRange first, scIChunkRange second)
@@ -112,7 +134,8 @@ void ChunkRangeBuilder::test()
 	auto cr1 = std::make_shared<ChunkRange>(nullptr, minMax3);
 	auto cr2 = std::make_shared<ChunkRange>(nullptr, minMax4);
 
-	auto result = isIntersects(cr1, cr2);
+	auto result = isAdjacent(cr1, cr2);
+	auto result2 = isIntersects(cr1, cr2);
 }
 
 static int testThings()
